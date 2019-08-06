8cdcd2176849664a2151dbab17c851909b4af7fe
idrummer83 <vodaadminwater@gmail.com>
2019-08-07 00:18:39 +0300

    heroku

--- taxi_park/settings.py
+++ taxi_park/settings.py
@@ -141,7 +141,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
-
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
